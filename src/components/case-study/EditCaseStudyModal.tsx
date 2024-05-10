779e2e5b75dbb5e3fca09304b7306f7403496882
Saajan Patel <saajanpatel@hotmail.com>
2024-05-10 22:40:31 +0100

    modal styling

--- src/components/case-study/EditCaseStudyModal.tsx
+++ src/components/case-study/EditCaseStudyModal.tsx
@@ -4,6 +4,7 @@ import {
   getCardPhotoFile,
   updateCaseStudy,
 } from "api/caseStudies";
+import { modalStyle } from "components/styles/model";
 import { fileStorage } from "lib/firebase/utils";
 import dynamic from "next/dynamic";
 import { useSnackbar } from "notistack";
@@ -14,20 +15,6 @@ type EditCaseStudyModalProps = {
   caseStudy: ICaseStudyForm;
   state: boolean;
   onClose: () => void;
-};
-
-const style = {
-  position: "absolute" as "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: "80vw",
-  height: "90vh",
-  bgcolor: "background.paper",
-  borderRadius: 2,
-  boxShadow: 24,
-  p: 4,
-  overflow: "auto",
 };
 
 const AddOrEditCaseStudy = dynamic(
@@ -88,7 +75,7 @@ const EditCaseStudyModal = ({
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
     >
-      <Box sx={style}>
+      <Box sx={modalStyle}>
         <AddOrEditCaseStudy
           onSubmitCaseStudy={onSubmit}
           defaultValues={defaultValues}
